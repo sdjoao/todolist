9c6fea58f06acf7509f30d9d0c98cf3694b17988
sdjoao <sdjoaoasl@hotmail.com>
2025-03-03 12:11:01 -0300

    feat(TarefaModel): Modelação da tabela tarefas e respectivos campos.

---
+++ backend/src/main/java/com/todolist/todolist/model/TarefaModel.java
@@ -0,0 +1,52 @@
+package com.todolist.todolist.model;
+
+import java.sql.Date;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tarefas")
+public class TarefaModel {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Boolean concluida = false;
+
+    @Column(nullable = false)
+    private LocalDateTime dataCriacao = LocalDateTime.now();
+
+    @Column(nullable = false)
+    private Date dataConclusao;
+
+    // getters e setters
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public Boolean isConcluida() { return concluida; }
+    public void setConcluida(Boolean concluida) { this.concluida = concluida; }
+
+    
+    public LocalDateTime getDataCriacao() { return dataCriacao; }
+    public void setDataCriacao(LocalDateTime dataCriacao) { this.dataCriacao = dataCriacao; }
+
+    // separar as datas
+    public Date getDataConclusao() { return dataConclusao; }
+    public void setDataConclusao(Date dataConclusao) { this.dataConclusao = dataConclusao; }
+    
+}
